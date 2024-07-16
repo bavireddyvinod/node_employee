0249cd3638f70a03c63e71ab15bedc0ca6e856cf
vinod <bavireddyvinod@gmail.com>
2024-07-16 18:16:30 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 const mongoose= require("mongoose")
+require("dotenv").config();
 //define the mongodb connection URL
-const mongoURL = "mongodb://localhost:27017/Database"
+// const mongoURL = "mongodb://localhost:27017/hotels";
+// const mongoURL = "mongodb+srv://bavireddyvinod:Vinod123456@cluster0.gu6bz5s.mongodb.net/";
+const mongoURL =process.env.MONGODB_URL
 //set up mongodb connection
-mongoose.connect(mongoURL,{
-    useNewUrlParser: true,
-    UseUnifiedTopology: true
-})
-
+mongoose.connect(mongoURL)
+// mongoose.connect(mongoURL,{
+//     useNewUrlParser: true,
+//     UseUnifiedTopology: true
+// })
 // Get the default connection
 // Mongoose maintains a default connection object representing the mongooes the mongodb connection
 const db = mongoose.connection;
@@ -17,9 +20,9 @@ db.on("connected", ()=>{
     console.log("connected to MongoDB srver");
 })
 db.on("error",()=>{
-    console.log("MongoDB connection error:", err);
+    console.log("MongoDB connection error:",err);
 })
 db.on("disconnected",()=>{
-    console.log("MongoDB disconnected  ");
+    console.log("MongoDB disconnected");
 });
 module.exports = db;
--- json.js
+++ json.js
@@ -13,13 +13,15 @@
 const express = require("express")
 const app = express();
 const db =require("./db")
+require("dotenv").config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 // const person =require("./models/person")
 // const menuItem = require("./models/Menuitem");
 // const emp = require("./models/employee");
 // const dept= require("./models/department");
-// const stu =require("./models/student"); 
+const stu =require("./models/student"); 
 // app.post("/person",async(req,res)=>{
 //     try{
 //         const data = req.body // assuming the reqest body contains the person data
@@ -172,14 +174,17 @@ app.use(bodyParser.json());
 // })
 // import the router files
 // const empRoutes= require("./routes/empRoutes");
-// // use the routers
+// // // use the routers
 // app.use("/emp",empRoutes);
 // const deptRoutes = require("./routes/deptRoutes");
 // app.use("/dept", deptRoutes);
 const stuRoutes = require("./routes/stuRoutes");
 app.use("/stu", stuRoutes);
-app.listen(3000,()=>{
+
+app.listen(PORT,()=>{
     console.log("listing on port 3000")
+// app.listen(3000,()=>{
+//     console.log("listing on port 3000")
 })
 
 
